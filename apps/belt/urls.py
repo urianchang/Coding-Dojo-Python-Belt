26ed7f3de0fa467c7837cdeedc05733cb6984a2e
Urian Chang <urianchang@gmail.com>
2017-02-24 09:53:47 -0800

    Login+Reg working

--- apps/belt/urls.py
+++ apps/belt/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
+    url(r'^travels$', views.welcome),
+    url(r'^logout$', views.logout),
+    # url(r'^delete$', views.delete),
+    url(r'^.+$', views.any)
 ]
